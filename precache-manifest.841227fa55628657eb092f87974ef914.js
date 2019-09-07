b92b358dc45a1cf9163e0277882b7dccdeaf65ab
andraaspar <andraaspar@gmail.com>
2019-09-08 00:32:35 +0200

    Updates

---
+++ precache-manifest.841227fa55628657eb092f87974ef914.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307a9813b9e6a8d701ffc5dff71eb25e",
+    "url": "/analog-ora/index.html"
+  },
+  {
+    "revision": "1b5e325fe39c85412073",
+    "url": "/analog-ora/static/css/main.3a2c09e7.chunk.css"
+  },
+  {
+    "revision": "4928011dd8bf883cef23",
+    "url": "/analog-ora/static/js/2.4ca74411.chunk.js"
+  },
+  {
+    "revision": "1b5e325fe39c85412073",
+    "url": "/analog-ora/static/js/main.37b65ad4.chunk.js"
+  },
+  {
+    "revision": "b1c4646dc7ef271a09d9",
+    "url": "/analog-ora/static/js/runtime~main.4af681d8.js"
+  }
+]);
