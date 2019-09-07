2880899db8864bde8fb97cd46bf1771a9ff890ec
andraaspar <andraaspar@gmail.com>
2019-09-08 00:20:39 +0200

    Add service worker.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppComp } from './AppComp'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<AppComp />, document.getElementById('root'))
+
+serviceWorker.register()
